6a34514fb323837c3068030301a865ca6668306c
azz-design <53371325+azz-design@users.noreply.github.com>
2020-02-06 21:09:08 +0530

    Project on IoT and MACHINE LEARNING

---
+++ IOT and ML project/final_thingspeak/final_thingspeak.ino
@@ -0,0 +1,56 @@
+#include <DHT.h>  
+#include <ESP8266WiFi.h>  
+#include <WiFiClient.h>  
+#include <ThingSpeak.h>
+
+
+
+#define DHTPIN D4  
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);  
+
+const char* ssid = "TP-Link_0106";  
+const char* password = "Basketballer??106";  
+WiFiClient client;  
+unsigned long myChannelNumber = 526842;  
+const char * myWriteAPIKey = "6PJBLZIJL9FEFCBF";  
+uint8_t temperature;
+void setup()  
+{  
+  Serial.begin(115200);   
+
+  dht.begin();  
+  delay(10); 
+  // Connect to WiFi network  
+  Serial.println();  
+  Serial.println();  
+  Serial.print("Connecting to ");  
+  Serial.println(ssid);  
+  WiFi.begin(ssid, password);  
+  while (WiFi.status() != WL_CONNECTED)  
+  {  
+   delay(500);  
+   Serial.print(".");  
+  }  
+  Serial.println("");  
+  Serial.println("WiFi connected");  
+  // Print the IP address  
+  Serial.println(WiFi.localIP());  
+  ThingSpeak.begin(client);  
+}  
+void loop()   
+{  
+  temperature = dht.readTemperature(); 
+  Serial.print("Temperature Value is :");  
+  Serial.print(temperature);  
+  Serial.println("C");  
+ 
+   
+  ThingSpeak.writeField(myChannelNumber,1,temperature,myWriteAPIKey); 
+  Serial.println("done"); 
+      
+  delay(3000); // ThingSpeak will only accept updates every 15 seconds.  
+} 
+
+
+
---
+++ IOT and ML project/iot_script_new/iot_script_new.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+const char *ssid = "SERVER NAME"; 
+const char *password = "SERVER PASSWORD";  
+const char* host = "script.google.com"; 
+const char* fingerprint = "29 fb b1 b3 ec 9a a3 c1 63 9b 17 87 25 e9 96 4a 59 2e 5e 4b";
+String url;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+}
+
+void loop() 
+{
+  Serial.print("connecting to ");
+  Serial.println(host);
+ 
+  WiFiClientSecure client;
+
+  const int httpPort = 443;
+  if (!client.connect(host, httpPort)) 
+  {
+    Serial.println("connection failed");
+    return;
+  }
+
+  float t = analogRead(A0);
+  
+  url = "/macros/s/AKfycbwx0nSG4sBPh1Zv1_agf3ORKlaAiUsk_0I3RfAYdWxZ9TWHXMYP/exec?func=addData&val="+ String(t);
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(500);
+  String section="header";
+  while(client.available())
+  {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.println();
+  Serial.println("closing connection");
+  delay(6000);
+}
